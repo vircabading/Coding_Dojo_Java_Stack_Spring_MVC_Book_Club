e6453a1f609b2dc6017b930e6521bcf0b3de67b6
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-12-11 20:23:42 -0800

    comment book bluc application

--- src/main/java/com/vcabading/bookclub/BookclubApplication.java
+++ src/main/java/com/vcabading/bookclub/BookclubApplication.java
@@ -3,6 +3,10 @@ package com.vcabading.bookclub;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/////////////////////////////////////////////////////////////////
+//	BOOK CLUB APPLICATION
+/////////////////////////////////////////////////////////////////
+
 @SpringBootApplication
 public class BookclubApplication {
 

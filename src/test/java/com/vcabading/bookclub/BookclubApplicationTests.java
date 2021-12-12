32b4444893195073ec9e9ffb0e7abba23fac89fb
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-12-11 20:16:51 -0800

    initial commit

---
+++ src/test/java/com/vcabading/bookclub/BookclubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vcabading.bookclub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookclubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

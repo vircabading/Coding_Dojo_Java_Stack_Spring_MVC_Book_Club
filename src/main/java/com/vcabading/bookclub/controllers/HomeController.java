15d9d3b97cdc1d92cde9a667b231a14f95ea8b62
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-12-11 21:27:22 -0800

    implement home controller

--- src/main/java/com/vcabading/bookclub/controllers/HomeController.java
+++ src/main/java/com/vcabading/bookclub/controllers/HomeController.java
@@ -1,16 +1,93 @@
 package com.vcabading.bookclub.controllers;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
 
-/////////////////////////////////////////////////////////////////
-//HOME CONTROLLER
-/////////////////////////////////////////////////////////////////
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+
+import com.vcabading.bookclub.models.LoginUser;
+import com.vcabading.bookclub.models.User;
+import com.vcabading.bookclub.services.UserService;
+
+////////////////////////////////////////////////////////////////////
+//	HOME CONTROLLER
+////////////////////////////////////////////////////////////////////
 
 @Controller
 public class HomeController {
-	@GetMapping("/")
-	public String index() {
-		return "index.jsp";
-	}
+	
+	//	//// FIELDS ////////////////////////////////////////////////
+    
+    @Autowired
+    private UserService userServ;
+    
+    //	//// SHOW //////////////////////////////////////////////////
+   
+    //	**** Display the root with Registration and Log-in Forms ***
+    @GetMapping("/")
+    public String index(Model model, HttpSession session) {
+    	if (session.getAttribute("user_id") != null) {		// If user is in session
+    		return "redirect:/dashboard";					//		re-route to dashboard
+    	}
+    	
+        model.addAttribute("newUser", new User());
+        model.addAttribute("newLogin", new LoginUser());
+        return "index.jsp";
+    }
+    
+    //	**** Display DASHBOARD *************************************
+    @GetMapping("/dashboard")
+    public String dashboard(Model model, HttpSession session) {
+    	if (session.isNew() || session.getAttribute("user_id") == null) {
+    		return "redirect:/";
+    	}
+    	User loggedInUser = this.userServ.retrieveUser((Long) session.getAttribute("user_id"));
+    	model.addAttribute("loggedInUser", loggedInUser);
+    	return "dashboard.jsp";
+    }
+    
+    //	//// CREATE ////////////////////////////////////////////////
+    
+    //	**** PUT: Register the New User ****************************
+
+    @PutMapping("/")
+    public String register(@Valid @ModelAttribute("newUser") User newUser, 
+            BindingResult result, Model model, HttpSession session) {
+        userServ.register(newUser, result);
+        if(result.hasErrors()) {
+            model.addAttribute("newLogin", new LoginUser());
+            return "index.jsp";
+        }
+        session.setAttribute("user_id", newUser.getId());
+        return "redirect:/dashboard";
+    }
+    
+    //	**** POST: Login the User ***********************************
+
+    @PostMapping("/")
+    public String login(@Valid @ModelAttribute("newLogin") LoginUser newLogin, 
+            BindingResult result, Model model, HttpSession session) {
+        User user = userServ.login(newLogin, result);
+        if(result.hasErrors()) {
+            model.addAttribute("newUser", new User());
+            return "index.jsp";
+        }
+        session.setAttribute("user_id", user.getId());
+        return "redirect:/dashboard";
+    }
+    
+    //	//// DELETE //////////////////////////////////////////////////
+    
+    @GetMapping("/logout")
+    public String logout(HttpSession session) {
+    	session.invalidate();					// 	Log the user out
+    	return "redirect:/";					//		then redirect to index 
+    }
 }
